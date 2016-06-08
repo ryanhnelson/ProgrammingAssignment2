063a04bfcbdf1f095539d54594f2d66e88740f36
Ryan Nelson <ryanhnelson@gmail.com>
2016-06-08 10:24:17 +0200

    Programming Assignment 2 Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,67 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
- # From Coursera:
- # makeCacheMatrix: This function creates a 
- # special "matrix" object that can cache its inverse.
+ # Matrix inversion and caching the inverse via function
+ # The goal here is to save time and power by first 
+ # caching a matrix and then computin its inverse.
+ # Need to:
+ #      1. Set a vector
+ #      2. Get the value of the vector
+ #      3. Set the mean
+ #      4. Get the mean value
+ # The second function then checks if this is cached
+ # and uses it if it is.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  # For X return an inverted matrix
+  # used for the cacheSolve function
+  
+  inv = NULL
+  set = function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(solve) inv <<- solve 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
-
-## Write a short comment describing this function
- # From Coursera:
- # cache solve: This function computes the inverse 
- # of the special "matrix" returned by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv = x$getinv()
+  # If inverse exists, pull cache and skip compute
+  if (!is.null(inv)){
+    message("Retrieving cached data")
+    return(inv)
+  }
+  # Else, calculate inverse 
+  mtest.data = x$get()
+  inv = solve(mtest.data, ...) # solve() inverts square matrices
+  # Sets inverse in cache.
+  x$setinv(inv)
+  
+  return(inv)
 }
+
+test = function(mtest){
+  # mtest: square matrix
+  
+  # First run, solves inver
+  CachedMatrix <- makeCacheMatrix(mtest)
+  start.time <- Sys.time()
+  solved <- cacheSolve(CachedMatrix)
+  duration <- Sys.time() - start.time
+  print(duration)
+  
+  #Second run, calls cache
+  start.time <- Sys.time()
+  solved <- cacheSolve(CachedMatrix)
+  duration2 <- Sys.time() - start.time
+  print(duration2)
+}
+
+
+ # Test Results
+ # mtest <- runif(1000000, min = -1000000, max = 1000000)
+ # mtest <- matrix(mtest, nrow = 1000, ncol = 1000)
+ # test(mtest)
+# Time difference of 0.1095681 secs
+# Retrieving cached data
+# Time difference of 0 secs
